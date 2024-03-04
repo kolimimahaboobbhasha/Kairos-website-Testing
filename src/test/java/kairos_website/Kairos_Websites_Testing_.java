4fd556a7010316720423812daedbab06272357ce
kolimimahaboobbhasha <121936001+kolimimahaboobbhasha@users.noreply.github.com>
2024-03-04 09:49:20 +0530

    Update Kairos_Websites_Testing_.java

--- src/test/java/kairos_website/Kairos_Websites_Testing_.java
+++ src/test/java/kairos_website/Kairos_Websites_Testing_.java
@@ -200,7 +200,7 @@ public class Kairos_Websites_Testing_ {
   	       email.addCc("pranathi.g@kairostech.com");
   		//email.addCc("kmahaboobbhasha@gmail.com");
   	email.addCc("prasad.k@kairostech.com");
-  	      //  email.addTo("kmahaboobbhasha@gmail.com");
+  	        email.addTo("kmahaboobbhasha@gmail.com");
   	     //   email.addTo("mahaboobbhasha17@gmail.com");
   	email.addTo("durgaprasad.b@kairostech.com");
 
